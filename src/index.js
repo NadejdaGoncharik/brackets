b4892bb4f63b6e473bea32f401c513e57243b312
NadejdaGoncharik <Nadin_2006_87@mail.ru>
2022-08-11 14:55:51 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0;
+  bracketsConfig.map(el => {
+    let brackets = el.join('');
+    if (str.indexOf(brackets) >= 0) {
+      str = str.replace(brackets, '');
+    } else {
+      i++;
+    }
+  });
+  if (i < bracketsConfig.length) {
+    return check(str, bracketsConfig);
+  } else {
+    return !str;
+  }
 }
